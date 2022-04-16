df679ef67c93def91c700d35bed60de1609da6e7
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-04-17 00:34:57 +0200

    simple node server

---
+++ Node.js Getting Started/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('server => req.headers', req.headers);
+    res.end('Hello world');
+});
+
+server.listen(8080, () => {
+    console.log('server is running');
+});
